c1a1b8b2380f11bbf4a4e077444c10a24b240865
Roody Pointjour <roody.jerry@yahoo.com>
2026-02-25 17:12:53 -0500

    add stm32 launchpad

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -44,11 +44,11 @@ function AboutMe(){
                     </p>
                     <ul className=' list-group list-group-flush' id="lst">
                     <li className='list-group-item list-group-item-dark'></li>
-                    <li className='list-group-item list-group-item-action list-group-item-light'><a id="python" href="https://rjpking92.pythonanywhere.com/" target="blank" rel="noreferrer">Python</a></li>
+                    <li className='list-group-item list-group-item-action list-group-item-light'><a id="python" href="https://rjpking92.pythonanywhere.com/" target="blank" rel="noreferrer">Machine Learning</a></li>
                     <li className='list-group-item list-group-item-action list-group-item-success'><a id="pi" href="https://rjpdev-pi-launchpad.web.app" target="blank" rel="noreferrer">Raspberry Pi</a></li>
                     <li className='list-group-item active list-group-item-action list-group-item-primary'><b>Bluetooth Low Energy (BLE)</b></li>
                     <li className='list-group-item list-group-item-action list-group-item-danger'><a id="esp32" href="https://rjpdev-esp32-launchpad.web.app" target="blank" rel="noreferrer">ESP32</a></li>
-                    <li className='list-group-item list-group-item-action list-group-item-warning'><b>STM32</b></li>
+                    <li className='list-group-item list-group-item-action list-group-item-warning'><a id="stm32" href="https://rjpdev-stm32-launchpad.web.app" target="blank" rel="noreferrer">STM32</a></li>
                     </ul>
                     </article>
             </div>
